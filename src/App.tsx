fd01098f0d9d27311266a5ad4d27039020398212
zehan12 <zehan9211@gmail.com>
2025-03-29 13:09:16 +0530

    feat: :art: integrate tailwind css

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ function App() {
 
   return (
     <>
-      <h1>Ellty Frontend Task</h1>
+      <h1 className="text-center underline text-3xl p-3 font-mono">Ellty Frontend Task</h1>
     </>
   )
 }
